087e40a53b3c291fe81a9c64700818cec3305b1b
Sam Urmy <oceanographerschoice@gmail.com>
2020-10-15 17:24:21 -0700

    simplify example.jl

--- test/example.jl
+++ test/example.jl
@@ -7,16 +7,16 @@ using Distributions
 using PDMats
 using Random
 
+Random.seed!(1)
 n = 2_000
-Random.seed!(0)
-pts = 100 * rand(n-4,2)
+pts = 100 * rand(n-4, 2)
 
 nodes = collect(kmeans(pts', 500).centers')
-corners = [-20 -20; 120 -20; 120 120; -20 120]
+# corners = [-20 -20; 120 -20; 120 120; -20 120]
 corners = [0 0; 100 0; 100 100; 0 100]
 
 mesh = create_mesh([nodes; corners])
-mesh = refine(mesh, divide_cell_into=2, voronoi=true)
+mesh = refine(mesh, divide_cell_into=4, voronoi=true)
 scatter(mesh.point[1, :], mesh.point[2, :])
 
 r = 15 # decorrelation range
@@ -30,7 +30,10 @@ length(nnz(Q.data)) / length(Q)
 U = cholesky(Q).PtL'
 
 x = U \ randn(mesh.n_point)
-surface(mesh.point[1, :], mesh.point[2, :], x, camera=(45, 80))
+surface(mesh.point[1, :], mesh.point[2, :], x, zlim=4*[-σ, σ], camera=(45, 80))
 
-xg = U \ rand(Gamma(0.02, 1000), mesh.n_point)
+xg = U \ rand(Gamma(0.1, 100), mesh.n_point)
 surface(mesh.point[1, :], mesh.point[2, :], xg, camera=(45, 80))
+
+D = MvNormalCanon(PDSparseMat(sparse(Q)))
+logpdf(D, x)

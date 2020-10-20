4392a67f489e0fecd66487e828cf1411603e22b0
Sam Urmy <oceanographerschoice@gmail.com>
2020-10-20 16:46:03 -0700

    renamed main module file

---
+++ src/SPDEPrecisionMatrices.jl
@@ -0,0 +1,25 @@
+module SPDEPrecisionMatrices
+
+using TriangleMesh
+using Interpolations
+using SparseArrays
+using LinearAlgebra
+using SpecialFunctions
+using Statistics
+using NearestNeighbors
+using ConcaveHull
+
+
+export component_matrices,
+    unscaled_precision_matrix,
+    calculate_κ,
+    calculate_τ,
+    precision_matrix,
+    make_border_points,
+    observation_matrix
+
+include("mesh_utils.jl")
+include("spde_precision_matrices.jl")
+
+
+end # module
